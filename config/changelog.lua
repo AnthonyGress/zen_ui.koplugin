87d003716b83498be982f80e888847c052766773
Anthony Gress <the.techx.guy@gmail.com>
2026-04-28 22:44:58 -0400

    update readme site

--- config/changelog.lua
+++ config/changelog.lua
@@ -12,8 +12,9 @@ return {
     ["1.0.2"] = {
         "Automatically disable incompatible plugins",
         "Access reader menu from page browser",
-        "Add changelog updated screen",
-        "Add new quick settings options, hide when plugin not installed",
+        'Add Report a "Report a Bug" button in Zen Settings > About',
+        "Add in-app changelogs",
+        "Add new quick settings buttons, hide when plugin not installed",
         "Add Bulgarian translation",
         "Bug fixes and performance improvements",
     },
