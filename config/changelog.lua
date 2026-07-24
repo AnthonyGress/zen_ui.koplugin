399fd707d78b7e3b7b3165d91575b76684e42b22
Anthony Gress <the.techx.guy@gmail.com>
2026-07-23 22:18:06 -0400

    improve reader theme in dark mode

--- config/changelog.lua
+++ config/changelog.lua
@@ -213,6 +213,7 @@ return {
     },
     ["2.5.3"] = {
         "Run plugin compat check immediately to prevent user install issues",
+        "Improve Reader Themes in dark mode",
         "Fix line graph y-axis truncating too early",
         "Add Bluetooth toggle for supported Kindles in Controls/Navbar/Launcher + status bar icon",
         "Add TBR instructions to docs",
