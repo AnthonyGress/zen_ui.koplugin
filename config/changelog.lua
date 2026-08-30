bafb3953f4e7fc08ade4192d1b84b3f0275f8b92
Anthony Gress <the.techx.guy@gmail.com>
2026-08-30 01:34:18 -0400

    update log

--- config/changelog.lua
+++ config/changelog.lua
@@ -316,8 +316,8 @@ return {
         "Fix reader highlight crash",
         "Fix some icons not rendering"
      },
-     ["3.3.9"] = {
-        "Add native metadata editor with EPUB backups and Hardcover autofill",
+     ["3.3.0"] = {
+        "Add native metadata editor and Hardcover autofill",
         "Add support for multiple quote files",
         "Add context to translations",
         "Add language in Zen Settings > About",
